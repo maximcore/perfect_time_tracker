60bee8dbae1311da6763d15a07116663ad6f47be
Maxim Borisenko <maximbeducation@gmail.com>
2021-11-30 00:48:31 +0300

    Authentification form details

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:perfect_time_tracker/common_widgets/show_alert_dialog.dart';
 import 'package:perfect_time_tracker/services/auth.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,6 +19,19 @@ class HomePage extends StatelessWidget {
     }
   }
 
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final didRequestSignOut = await showAlertDialog(
+      context,
+      content: 'Are you sure that you want to Sign out?',
+      title: 'Sign out',
+      cancelActionText: 'Cancel',
+      defaultActionText: 'Sign Out',
+    );
+    if (didRequestSignOut) {
+      _signOut();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +39,7 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
         actions: [
           TextButton(
-            onPressed: _signOut,
+            onPressed: () => _confirmSignOut(context),
             child: const Text(
               'Sign Out',
               style: TextStyle(
